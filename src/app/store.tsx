13d2af08e94e1a3bc188f3cf2fff21bb354a36e1
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-07-26 06:40:25 -0700

    Define the type of the variable "store" in the file "store.tsx".

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -1,7 +1,7 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { Store, configureStore } from "@reduxjs/toolkit";
 
 
-export const store = configureStore({
+export const store: Store = configureStore({
 
   reducer: {
 
