bb7cbe412f5dbf6c123568e99f5c91f825598d24
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-08-01 17:57:00 -0700

    Update "App.tsx" and "App.scss" such that the offline and online pages are correctly being displayed, depending on the value of "appStatus"

--- src/App.tsx
+++ src/App.tsx
@@ -18,12 +18,27 @@ function App(): JSX.Element {
 
 
 
-  // useEffect(() => {
+  useEffect(() => {
 
-  //   (document.getElementById("app-container") as HTMLElement)
-  //     .style.background = appStatus ? "white" : "$pg-background";
+    const appContainer: null | HTMLElement = document.getElementById("app-container");
 
-  // }, [appStatus]);
+    if (appStatus) {
+
+      (appContainer as HTMLElement).style.background = "hsl(220, 50%, 60%)";
+      (appContainer as HTMLElement).style.border = "25px double hsl(220, 50%, 30%)";
+      (appContainer as HTMLElement).style.justifyContent = "center";
+      (appContainer as HTMLElement).style.alignItems = "center";
+
+    } else {
+
+      (appContainer as HTMLElement).style.background = "hsl(0, 50%, 50%)";
+      (appContainer as HTMLElement).style.border = "none";
+      (appContainer as HTMLElement).style.justifyContent = "start";
+      (appContainer as HTMLElement).style.alignItems = "start";
+
+    }
+
+  }, [appStatus]);
 
 
 
