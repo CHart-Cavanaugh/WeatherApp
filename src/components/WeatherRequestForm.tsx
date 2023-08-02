f0c8110a49a4df5733665da3799a11b0625357f5
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-08-01 17:00:39 -0700

    Import code from CodePen to "WeatherRequestForm.tsx"

---
+++ src/components/WeatherRequestForm.tsx
@@ -0,0 +1,12 @@
+function WeatherRequestForm() {
+
+  return (
+
+    <aside id="weather-request-sidebar">
+      <form id="weather-request-form"></form>
+      <div id="weather-request-history"></div>
+    </aside>
+
+  );
+
+}
