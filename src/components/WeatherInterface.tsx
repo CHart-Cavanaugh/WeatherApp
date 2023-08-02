ccb5ddbf08298a3390c18142f563cfbd89767576
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-08-01 17:15:30 -0700

    Import code from CodePen to "WeatherInterface.tsx"

---
+++ src/components/WeatherInterface.tsx
@@ -0,0 +1,13 @@
+function WeatherInterface() {
+
+  return (
+
+    <main id="weather-interface">
+      <WeatherRequestForm />
+      <WeatherInformation />
+      {window.innerWidth < 768 ? <InterfaceTabs /> : null}
+    </main>
+
+  );
+
+}
