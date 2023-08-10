33af2e71fb6518bc5a86ae28d7a9260aa2af6314
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-08-09 18:59:37 -0700

    Add 2 inputs with labels for "State" and "City" to the form element of the "WeatherRequestForm" component.

--- src/components/WeatherRequestForm.tsx
+++ src/components/WeatherRequestForm.tsx
@@ -3,7 +3,17 @@ export function WeatherRequestForm(): JSX.Element {
   return (
 
     <aside id="weather-request-sidebar">
-      <form id="weather-request-form"></form>
+      <form id="weather-request-form">
+        <label htmlFor="state-input">
+          State
+          <input type="text" id="state-input" name="state" />
+        </label>
+        <label htmlFor="city-input">
+          City
+          <input type="text" id="city-input" name="city" />
+        </label>
+        <button>Submit</button>
+      </form>
       <div id="weather-request-history"></div>
     </aside>
 
