32d1fe4e8c0d4a5c0a44c161f650f4a9e8dff5d3
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-08-11 09:51:05 -0700

    Do the following:
    - Define "showElement" and "hideElement" as
    functions that shows or hides the specified
    element.
    
    - Pass "showElement" and "hideElement" as props
    to the "InterfaceTabs" component.

--- src/components/WeatherInterface.tsx
+++ src/components/WeatherInterface.tsx
@@ -1,17 +1,75 @@
 import { InterfaceSidebar } from "./InterfaceSidebar";
 import { WeatherInformation } from "./WeatherInformation";
 import { InterfaceTabs } from "./InterfaceTabs";
+import { useEffect } from "react";
 
 
 
 export function WeatherInterface(): JSX.Element {
+
+  const showElement = (elementName: string) => {
+
+    let element: HTMLElement | null = null;
+
+
+
+    if (!(elementName === "weather-info" || elementName === "weather-request-sidebar"))
+      return;
+
+    element = document.getElementById(elementName);
+
+    if (elementName === "weather-info") {
+
+      (element as HTMLElement).style.display = "block";
+
+    } else if (elementName === "weather-request-sidebar") {
+
+      (element as HTMLElement).style.display = "flex";
+
+    }
+
+  };
+
+  const hideElement = (elementName: string) => {
+
+    let element: HTMLElement | null = null;
+
+
+
+    if (!(elementName === "weather-info" || elementName === "weather-request-sidebar"))
+      return;
+
+    element = document.getElementById(elementName);
+    (element as HTMLElement).style.display = "none";
+
+  };
+
+
+
+  // useEffect(() => {
+
+  //   window.addEventListener("resize", () => {
+
+  //     if (window.innerHeight >= 768) {
+  //       showElement("weather-info");
+  //       showElement("weather-request-sidebar");
+  //     } else {
+  //       hideElement("weather-info");
+  //       showElement("weather-request-sidebar");
+  //     }
+
+  //   })
+
+  // }, []);
+
+
 
   return (
 
     <main id="weather-interface">
       <InterfaceSidebar />
       <WeatherInformation />
-      {window.innerWidth < 768 ? <InterfaceTabs /> : null}
+      {window.innerWidth < 768 ? <InterfaceTabs showElement={showElement} hideElement={hideElement} /> : null}
     </main>
 
   );
