f4145161fe4b392c3b34d14e92487657d986d164
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-08-17 18:26:33 -0700

    Fix the issue where the "interface-tabs" element wasn't correctly resetting when the browser was resized from a width of >= 768 pixels to a width of < 768 pixels.

--- src/components/InterfaceTabs.tsx
+++ src/components/InterfaceTabs.tsx
@@ -3,53 +3,19 @@ import { useEffect } from "react";
 
 
 interface ChildProps {
-  showElement: (elementName: string) => void,
-  hideElement: (elementName: string) => void,
+
+  setTabOneActive: () => void,
+  setTabTwoActive: () => void,
+
 };
 
 
 
 export function InterfaceTabs(props: ChildProps): JSX.Element {
 
-  const setTabOneActive = () => {
-
-    const interfaceTabOne: HTMLElement = document.getElementById("interface-tab-1") as HTMLElement;
-    const interfaceTabTwo: HTMLElement = (document.getElementById("interface-tab-2") as HTMLElement);
-    const activeBg: string = "hsl(280, 70%, 60%)";
-    const inactiveBg: string = "hsl(280, 50%, 30%)";
-
-
-
-    interfaceTabTwo.style.background = inactiveBg;
-    interfaceTabOne.style.background = activeBg;
-
-    props.hideElement("weather-info");
-    props.showElement("weather-request-sidebar");
-
-  };
-
-  const setTabTwoActive = () => {
-
-    const interfaceTabOne: HTMLElement = document.getElementById("interface-tab-1") as HTMLElement;
-    const interfaceTabTwo: HTMLElement = (document.getElementById("interface-tab-2") as HTMLElement);
-    const activeBg: string = "hsl(280, 70%, 60%)";
-    const inactiveBg: string = "hsl(280, 50%, 30%)";
-
-
-
-    interfaceTabOne.style.background = inactiveBg;
-    interfaceTabTwo.style.background = activeBg;
-
-    props.hideElement("weather-request-sidebar");
-    props.showElement("weather-info");
-
-  };
-
-
-
   useEffect(() => {
 
-    setTabOneActive();
+    props.setTabOneActive();
 
   }, []);
 
@@ -62,14 +28,14 @@ export function InterfaceTabs(props: ChildProps): JSX.Element {
         id="interface-tab-1"
         key="1"
         onClick={() => {
-          setTabOneActive();
+          props.setTabOneActive();
         }}
       ></div>
       <div
         id="interface-tab-2"
         key="2"
         onClick={() => {
-          setTabTwoActive();
+          props.setTabTwoActive();
         }}
       ></div>
     </footer>
--- src/components/WeatherInterface.tsx
+++ src/components/WeatherInterface.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useEffect } from "react";
 import { InterfaceSidebar } from "./InterfaceSidebar";
 import { WeatherInformation } from "./WeatherInformation";
@@ -6,74 +5,127 @@ import { InterfaceTabs } from "./InterfaceTabs";
 
 
 
+const interfaceControls = {
+
+  general: {
+
+    showElement: (elementName: string): void => {
+
+      let element: HTMLElement | null = null;
+
+
+
+      if (!(elementName === "weather-info" || elementName === "weather-request-sidebar"))
+        return;
+
+      element = document.getElementById(elementName);
+
+      if (elementName === "weather-info") {
+
+        (element as HTMLElement).style.display = "block";
+
+      } else if (elementName === "weather-request-sidebar") {
+
+        (element as HTMLElement).style.display = "flex";
+
+      }
+
+    },
+    hideElement: (elementName: string): void => {
+
+      let element: HTMLElement | null = null;
+
+
+
+      if (!(elementName === "weather-info" || elementName === "weather-request-sidebar"))
+        return;
+
+      element = document.getElementById(elementName);
+      (element as HTMLElement).style.display = "none";
+
+    },
+
+  },
+
+  interfaceTabs: {
+
+    setTabOneActive: () => {
+
+      const interfaceTabOne: HTMLElement = document.getElementById("interface-tab-1") as HTMLElement;
+      const interfaceTabTwo: HTMLElement = (document.getElementById("interface-tab-2") as HTMLElement);
+      const activeBg: string = "hsl(280, 70%, 60%)";
+      const inactiveBg: string = "hsl(280, 50%, 30%)";
+
+
+
+      interfaceTabTwo.style.background = inactiveBg;
+      interfaceTabOne.style.background = activeBg;
+
+      interfaceControls.general.hideElement("weather-info");
+      interfaceControls.general.showElement("weather-request-sidebar");
+
+    },
+    setTabTwoActive: () => {
+
+      const interfaceTabOne: HTMLElement = document.getElementById("interface-tab-1") as HTMLElement;
+      const interfaceTabTwo: HTMLElement = (document.getElementById("interface-tab-2") as HTMLElement);
+      const activeBg: string = "hsl(280, 70%, 60%)";
+      const inactiveBg: string = "hsl(280, 50%, 30%)";
+
+
+
+      interfaceTabOne.style.background = inactiveBg;
+      interfaceTabTwo.style.background = activeBg;
+
+      interfaceControls.general.hideElement("weather-request-sidebar");
+      interfaceControls.general.showElement("weather-info");
+
+    },
+
+  }
+
+};
+
+
 
 export function WeatherInterface(): JSX.Element {
 
-  const [showTabs, setShowTabs]: [boolean, React.Dispatch<React.SetStateAction<boolean>>] = useState(false);
-
-  const showElement = (elementName: string): void => {
-
-    let element: HTMLElement | null = null;
-
-
-
-    if (!(elementName === "weather-info" || elementName === "weather-request-sidebar"))
-      return;
-
-    element = document.getElementById(elementName);
-
-    if (elementName === "weather-info") {
-
-      (element as HTMLElement).style.display = "block";
-
-    } else if (elementName === "weather-request-sidebar") {
-
-      (element as HTMLElement).style.display = "flex";
-
-    }
-
-  };
-
-  const hideElement = (elementName: string): void => {
-
-    let element: HTMLElement | null = null;
-
-
-
-    if (!(elementName === "weather-info" || elementName === "weather-request-sidebar"))
-      return;
-
-    element = document.getElementById(elementName);
-    (element as HTMLElement).style.display = "none";
-
-  };
-
-
-
   useEffect(() => {
+
+    const interfaceTabs: HTMLElement = document.getElementById("interface-tabs") as HTMLElement;
+
+
 
     window.addEventListener("resize", () => {
 
       if (window.innerWidth >= 768) {
 
-        showElement("weather-info");
-        showElement("weather-request-sidebar");
-        setShowTabs(false);
+        interfaceControls.general.showElement("weather-info");
+        interfaceControls.general.showElement("weather-request-sidebar");
+        interfaceTabs.style.display = "none";
 
       } else {
 
-        hideElement("weather-info");
-        showElement("weather-request-sidebar");
-        setShowTabs(true);
+        if (interfaceTabs.style.display === "none") {
+
+          interfaceControls.general.hideElement("weather-info");
+          interfaceControls.general.showElement("weather-request-sidebar");
+          interfaceControls.interfaceTabs.setTabOneActive();
+
+        }
+
+        interfaceTabs.style.display = "flex";
 
       }
 
     });
 
+
+
     if (window.innerWidth < 768)
-      setShowTabs(true);
+      interfaceTabs.style.display = "flex";
     else
-      setShowTabs(false);
+      interfaceTabs.style.display = "none";
 
 
 
@@ -83,19 +135,25 @@ export function WeatherInterface(): JSX.Element {
 
         if (window.innerWidth >= 768) {
 
-          showElement("weather-info");
-          showElement("weather-request-sidebar");
-          setShowTabs(false);
+          interfaceControls.general.showElement("weather-info");
+          interfaceControls.general.showElement("weather-request-sidebar");
+          interfaceTabs.style.display = "none";
 
         } else {
 
-          hideElement("weather-info");
-          showElement("weather-request-sidebar");
-          setShowTabs(true);
+          if (interfaceTabs.style.display === "none") {
+
+            interfaceControls.general.hideElement("weather-info");
+            interfaceControls.general.showElement("weather-request-sidebar");
+            interfaceControls.interfaceTabs.setTabOneActive();
+
+          }
+
+          interfaceTabs.style.display = "flex";
 
         }
 
-      })
+      });
 
     };
 
@@ -106,7 +164,10 @@ export function WeatherInterface(): JSX.Element {
   return (
 
     <main id="weather-interface">
-      {showTabs ? <InterfaceTabs showElement={showElement} hideElement={hideElement} /> : null}
+      <InterfaceTabs
+        setTabOneActive={interfaceControls.interfaceTabs.setTabOneActive}
+        setTabTwoActive={interfaceControls.interfaceTabs.setTabTwoActive}
+      />
       <InterfaceSidebar />
       <WeatherInformation />
     </main>
