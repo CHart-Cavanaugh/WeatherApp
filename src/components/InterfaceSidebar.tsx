c505543f3fb188e5b17080145dc5698ef8a7860d
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-08-18 22:33:16 -0700

    Update #1: Update the "InterfaceSidebar" component so that the "weather-request-form" element only contains 1 input followed by 1 button, on the same line.
    Update #2: Update "App.scss" so that the appearance
    of the "InterfaceSidebar" component looks good with
    the changes made from "Update #1".

--- src/components/InterfaceSidebar.tsx
+++ src/components/InterfaceSidebar.tsx
@@ -4,15 +4,10 @@ export function InterfaceSidebar(): JSX.Element {
 
     <aside id="weather-request-sidebar">
       <form id="weather-request-form">
-        <label htmlFor="state-input">
-          State
-          <input type="text" id="state-input" name="state" />
-        </label>
-        <label htmlFor="city-input">
-          City
-          <input type="text" id="city-input" name="city" />
-        </label>
-        <button>Submit</button>
+        <div>
+          <input type="search" id="city-input" name="city" placeholder="City" />
+          <button>Submit</button>
+        </div>
       </form>
       <div id="weather-request-history"></div>
     </aside>
