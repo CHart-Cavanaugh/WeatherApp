fc6824a942f2bbc137e8597f1a9a701e448260e5
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-08-18 22:52:00 -0700

    Add "Weather Information Notes" as comments at the top of "WeatherInformation.tsx".

--- src/components/WeatherInformation.tsx
+++ src/components/WeatherInformation.tsx
@@ -1,3 +1,36 @@
+//Weather Information Notes
+/*
+
+  Weather Information Header
+
+    0) [START]
+    1) Content:
+
+      0) [START]
+      1) Location (City, State/Province/Null, Country)
+      2) Current Temperature (C/F)
+      3) High / Low Temperature (C/F)
+      4) Wind Speed
+      n) [END]
+
+    n) [END]
+
+  Weather Information Hourly Forecast
+
+    0) [START]
+    1) Content:
+
+      0) [START]
+      n) [END]
+
+    n) [END]
+
+  END
+
+*/
+
+
+
 export function WeatherInformation(): JSX.Element {
 
   return (
