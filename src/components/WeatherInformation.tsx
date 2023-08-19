38f37078b99a9eb813c3bb26c846927343cd86b8
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-08-19 14:51:01 -0700

    Add some basic information to the header of the "WeatherInformation" component.

--- src/components/WeatherInformation.tsx
+++ src/components/WeatherInformation.tsx
@@ -38,7 +38,81 @@ export function WeatherInformation(): JSX.Element {
   return (
 
     <section id="weather-info">
-      <header></header>
+      <header>
+        <section>
+          <h3 id="location-label"
+            className="weather-info-label"
+          >
+            someLocation
+          </h3>
+        </section>
+        <section>
+          <div id="temp-info-container">
+            <h4
+              className="weather-info-label"
+              id="current-temp"
+            >
+              Temperature:
+            </h4>
+            <div
+              id="temp-unit-labels"
+            >
+              <p
+                className="temp-unit"
+                id="temp-unit-celcius"
+              >
+                C
+              </p>
+              <p
+                className="temp-unit"
+                id="temp-unit-celcius"
+              >
+                F
+              </p>
+            </div>
+            <h1>0</h1>
+          </div>
+          <div id="wind-info-container">
+            <h4 id="wind-dir-label"
+              className="weather-info-label"
+            >
+              Wind Direction: <span>"E"</span>
+            </h4>
+            <h4 id="wind-speed-label"
+              className="weather-info-label"
+
+            >
+              Wind Speed: <span>0</span> mph
+            </h4>
+          </div>
+          <div id="displaced-labels">
+            <h4
+              className="weather-info-label"
+              id="current-humidity"
+            >
+              Humidity: <span>0</span> %
+            </h4>
+            <h4
+              className="weather-info-label"
+              id="uv-index-label"
+            >
+              UV Index: <span>0</span>
+            </h4>
+            <h4
+              className="weather-info-label"
+              id="pressure-label"
+            >
+              Pressure: <span>0.00</span> in
+            </h4>
+            <h4
+              className="weather-info-label"
+              id="visibility-label"
+            >
+              Visibility: <span>0.0</span> mi
+            </h4>
+          </div>
+        </section>
+      </header>
       <section></section>
     </section>
 
