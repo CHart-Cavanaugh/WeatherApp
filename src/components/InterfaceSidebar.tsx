8d8bef7e753140eeba86e88ac96d063bbc66893a
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-08-18 23:54:34 -0700

    Update the "InterfaceSidebar" component so that the "weather-request-form" element will request information from the "weatherapi" api based on the value of "weather-request-input" input element when the "submit" button is pressed or clicked on.

--- src/components/InterfaceSidebar.tsx
+++ src/components/InterfaceSidebar.tsx
@@ -1,11 +1,55 @@
+import { useEffect } from 'react';
+
+
+
 export function InterfaceSidebar(): JSX.Element {
+
+  let responseJSON: null | JSON = null;
+  const handleSubmit = (e: Event) => {
+
+    const weatherReqInput: HTMLElement | null = document.getElementById("weather-request-input");
+
+    e.preventDefault();
+
+    fetch(`http://api.weatherapi.com/v1/current.json?key=d790a10e5fe74e8db6260020231908&q=${(weatherReqInput as HTMLInputElement).value}&aqi=no`)
+      .then((response) => response.json())
+      .then((json) => {
+
+        console.log(json);
+        responseJSON = json;
+
+      });
+
+  };
+
+
+
+  useEffect(() => {
+
+    const requestForm: HTMLElement | null = document.getElementById("weather-request-form");
+
+
+
+    requestForm?.addEventListener("submit", handleSubmit);
+
+
+
+    return () => {
+
+      requestForm?.removeEventListener("submit", handleSubmit);
+
+    }
+
+  }, []);
+
+
 
   return (
 
     <aside id="weather-request-sidebar">
       <form id="weather-request-form">
         <div>
-          <input type="search" id="city-input" name="city" placeholder="City" />
+          <input type="search" id="weather-request-input" name="city" placeholder="City or Postal Code" />
           <button>Submit</button>
         </div>
       </form>
