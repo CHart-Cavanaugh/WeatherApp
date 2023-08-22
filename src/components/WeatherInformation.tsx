db88f6f220766b089e8666fe28c08934e1976359
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-08-21 18:28:11 -0700

    Update #1: Reduce nesting in "_weatherInformation.scss"
    Update #2: Optimize class names and style rules
    for the "WeatherInformation" component.

--- src/components/WeatherInformation.tsx
+++ src/components/WeatherInformation.tsx
@@ -41,7 +41,7 @@ export function WeatherInformation(): JSX.Element {
       <header>
         <section id="location-info">
           <h3 id="location-val"
-            className="weather-info-label"
+            className="info-label"
           >
             someLocation
           </h3>
@@ -49,7 +49,7 @@ export function WeatherInformation(): JSX.Element {
         <section id="main-info">
           <div id="temp-info">
             <h4 id="current-temp-label"
-              className="weather-info-label"
+              className="info-label"
             >
               Temperature:
             </h4>
@@ -65,69 +65,83 @@ export function WeatherInformation(): JSX.Element {
                 F
               </p>
             </div>
-            <p id="current-temp-val">
+            <p id="current-temp-val"
+              className="info-val"
+            >
               0
             </p>
           </div>
           <div id="wind-info">
             <div id="wind-dir-info">
               <h4 id="wind-dir-label"
-                className="weather-info-label"
+                className="info-label"
               >
                 Wind Direction:
               </h4>
-              <p id="wind-dir-val">
+              <p id="wind-dir-val"
+                className="info-val"
+              >
                 "E"
               </p>
             </div>
             <div id="wind-speed-info">
               <h4 id="wind-speed-label"
-                className="weather-info-label"
+                className="info-label"
               >
                 Wind Speed:
               </h4>
-              <p id="wind-speed-val">
+              <p id="wind-speed-val"
+                className="info-val"
+              >
                 <span>0</span> <span>mph</span>
               </p>
             </div>
           </div>
           <div id="humidity-info">
             <h4 id="humidity-label"
-              className="weather-info-label"
+              className="info-label"
             >
               Humidity:
             </h4>
-            <p id="humidity-val">
+            <p id="humidity-val"
+              className="info-val"
+            >
               <span>0</span> <span>%</span>
             </p>
           </div>
           <div id="uv-index-info">
             <h4 id="uv-index-label"
-              className="weather-info-label"
+              className="info-label"
             >
               UV Index:
             </h4>
-            <p id="uv-index-val">
+            <p id="uv-index-val"
+              className="info-val"
+            >
               0
             </p>
           </div>
           <div id="pressure-info">
             <h4 id="pressure-label"
-              className="weather-info-label"
+              className="info-label"
             >
               Pressure:
             </h4>
-            <p id="pressure-val">
+            <p id="pressure-val"
+              className="info-val"
+            >
               <span>0.00</span> <span>in</span>
             </p>
           </div>
           <div id="visibility-info">
             <h4 id="visibility-label"
-              className="weather-info-label"
+              className="info-label"
             >
               Visibility:
             </h4>
-            <p id="visibility-val">
+            <p id="visibility-val"
+              className="info-val"
+            >
               <span>0.0</span> <span>mi</span>
             </p>
           </div>
