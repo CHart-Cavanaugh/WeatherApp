4fd25c959224e2005ebf9c6e8b3f672b45c83d4e
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-08-29 04:45:27 -0700

    Update #1: Change the return value for the "HourlyForecastContainer" component so that it returns a "section" element with an id of "hourly-forecast-container" and no content.
    Update #2: Define a type aliases for the data being
    displayed in the "HourlyForecastContainer" component.
    
    Update #3: Import "useState" and use it to define an
    array of HourForecasts as part of the component's local
    state and give it a temporary value.
    
    Update #4: Without actually defining the logic of the
    function, define a function for refreshing hour
    forecasts and give it a single paramater which is a
    callback function for refreshing the info of a single
    hour forecast.
    
    Update #5: Define a temporary forecast refresh method
    to use when the "ForcastInfoType" type alias is set to
    "number".

--- src/components/HourlyForecastContainer.tsx
+++ src/components/HourlyForecastContainer.tsx
@@ -1,7 +1,58 @@
-function HourlyForecastContainer(): JSX.Element {
+import { useState } from 'react';
+
+
+
+type ForecastInfoType = number;
+type HourForecasts = [
+
+  hourOne: ForecastInfoType,
+  hourTwo: ForecastInfoType,
+  hourThree: ForecastInfoType,
+  hourFour: ForecastInfoType,
+  hourFive: ForecastInfoType
+
+];
+
+
+const TEMP_FORECASTS: HourForecasts = [1, 2, 3, 4, 5];
+
+
+
+const TEMP_FORECAST_REFRESH: ((val: number) => number) = (
+
+  (val: number) => val === 12 ? 0 : val + 1
+
+);
+
+
+
+
+export function HourlyForecastContainer(): JSX.Element {
+
+  const [forecasts, setForecasts]: [HourForecasts, Function] = useState(TEMP_FORECASTS);
+
+
+
+  function refreshHourForecasts(refreshMethod: (val: ForecastInfoType) => ForecastInfoType): void {
+
+    //Notes:
+    /*
+
+      - Create a copy of forecasts and store it in a temporary array.
+      - Update each of the values in the temporary array using the refresh method.
+      - Update forecasts using the updated temporary array.
+
+    */
+
+  }
+
+
 
   return (
-    <></>
+    <section id="hourly-forecast-container">
+
+    </section>
   );
 
 }
+
