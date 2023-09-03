ae247221a395c55e2eeac3e67bcc07e668a2fe22
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-09-03 13:32:47 -0700

    Define a basic structure and style for the "WeatherRequestHistory" component.

--- src/features/weatherRequest/WeatherRequestHistory.tsx
+++ src/features/weatherRequest/WeatherRequestHistory.tsx
@@ -2,7 +2,11 @@ export function WeatherRequestHistory(): JSX.Element {
 
   return (
 
-    <div id="weather-request-history"></div>
+    <div id="weather-request-history">
+      <ul id="request-history-list">
+        <li>someCity, someCountry</li>
+      </ul>
+    </div>
 
   );
 
