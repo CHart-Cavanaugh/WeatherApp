baf4bef2f61c344b1ee83caed75e197bdf0ce7e4
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-09-05 07:26:50 -0700

    Add "apiResponses" to the redux store as part of the app state.

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -1,9 +1,13 @@
 import { Store, configureStore } from "@reduxjs/toolkit";
+import apiResponseReducer from "./slices/apiResponsesSlice";
+
 
 
 export const store: Store = configureStore({
 
   reducer: {
+
+    apiResponses: apiResponseReducer,
 
   },
 
