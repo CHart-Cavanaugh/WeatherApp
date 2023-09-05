c89106914c181912f496a0f33bd50d31a07fbd78
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-09-04 21:45:33 -0700

    Update the API request link to include forecast data for 2 days (including hourly forecasts).

--- src/features/weatherRequest/WeatherRequestForm.tsx
+++ src/features/weatherRequest/WeatherRequestForm.tsx
@@ -11,7 +11,7 @@ export function WeatherRequestForm(): JSX.Element {
 
     e.preventDefault();
 
-    fetch(`http://api.weatherapi.com/v1/current.json?key=d790a10e5fe74e8db6260020231908&q=${(weatherReqInput as HTMLInputElement).value}&aqi=no`)
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=d790a10e5fe74e8db6260020231908&q=${(weatherReqInput as HTMLInputElement).value}&days=2&aqi=no&alerts=no`)
       .then((response) => response.json())
       .then((json) => {
 
