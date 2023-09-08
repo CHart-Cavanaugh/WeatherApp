f3c37ceaf1e6997462f91c0c10eda8bd8b3ff93e
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-09-08 09:12:29 -0700

    Temporarily change the "WeatherRequestHistory" component so that the list items created correspond with the "testSubmissions" array from the redux store instead of an array of API responses from the redux store.

--- src/features/weatherRequest/WeatherRequestHistory.tsx
+++ src/features/weatherRequest/WeatherRequestHistory.tsx
@@ -3,30 +3,56 @@ import { useSelector } from 'react-redux';
 
 export function WeatherRequestHistory(): JSX.Element {
 
-  const apiResponses: any[] = useSelector((state: { apiResponses: [] }) => state.apiResponses)
+  const apiResponses: any[] = useSelector((state: { apiResponses: [] }) => state.apiResponses);
+  const testSubmissions: string[] = useSelector((state: { testSubmissions: string[] }) => state.testSubmissions);
+  function getListItems(): JSX.Element[] {
+
+    return testSubmissions.map((value, index) => {
+
+      return (
+
+        <li key={"request-" + (index + 1)}>
+          {(index + 1) + ") " + testSubmissions[index]}
+        </li>
+
+      );
+
+    })
+
+
+
+    /* Get List Items Based On API Responses
+  
+      return apiResponses.map((value, index) => {
+  
+        const city = apiResponses[index].location.name;
+        let country = apiResponses[index].location.country;
+  
+  
+  
+        country = country === "United States of America" ? "USA" : country;
+  
+  
+  
+        return (
+          <li key={"request-" + (index + 1)}>
+            {`${city}, ${country}`}
+          </li>
+        );
+  
+      })
+  
+    */
+
+  }
+
+
 
   return (
 
     <div id="weather-request-history">
       <ul id="request-history-list">
-        {apiResponses.map((value, index) => {
-
-          const city = apiResponses[index].location.name;
-          let country = apiResponses[index].location.country;
-
-
-
-          country = country === "United States of America" ? "USA" : country;
-
-
-
-          return (
-            <li key={"request-" + (index + 1)}>
-              {`${city}, ${country}`}
-            </li>
-          );
-
-        })}
+        {getListItems()}
       </ul>
     </div>
 
