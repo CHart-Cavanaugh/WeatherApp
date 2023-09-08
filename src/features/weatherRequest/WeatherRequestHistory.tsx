ab5ba5997e07d74b1b5fea17903c1ec081e00120
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-09-08 01:10:34 -0700

    Update "WeatherRequestHistory.tsx" so that list of of the "#weather-request-history" element contains list items that correspond to API responses stored in the redux store (make it so that each list item displays "city, country" based on the API response that the item is associated with).

--- src/features/weatherRequest/WeatherRequestHistory.tsx
+++ src/features/weatherRequest/WeatherRequestHistory.tsx
@@ -1,10 +1,32 @@
+import { useSelector } from 'react-redux';
+
+
 export function WeatherRequestHistory(): JSX.Element {
+
+  const apiResponses: any[] = useSelector((state: { apiResponses: [] }) => state.apiResponses)
 
   return (
 
     <div id="weather-request-history">
       <ul id="request-history-list">
-        <li>someCity, someCountry</li> {/* REMOVE LATER. FOR TESTING PURPOSES ONLY */}
+        {apiResponses.map((value, index) => {
+
+          const city = apiResponses[index].location.name;
+          let country = apiResponses[index].location.country;
+
+
+
+          country = country === "United States of America" ? "USA" : country;
+
+
+
+          return (
+            <li key={"request-" + (index + 1)}>
+              {`${city}, ${country}`}
+            </li>
+          );
+
+        })}
       </ul>
     </div>
 
