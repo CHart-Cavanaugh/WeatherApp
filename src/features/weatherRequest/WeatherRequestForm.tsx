077f0c0a563e63af52fe847ac31a3ee57c25ea4c
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-09-09 10:04:12 -0700

    Update "WeatherRequestForm.tsx" so that form submissions update the API responses array in the redux store and make it so that form submissions handle duplicate responses and list item selections in the same way it was being handled for testSubmissions.

--- src/features/weatherRequest/WeatherRequestForm.tsx
+++ src/features/weatherRequest/WeatherRequestForm.tsx
@@ -21,39 +21,62 @@ export function WeatherRequestForm(): JSX.Element {
 
     e.preventDefault();
 
-    if (testSubmissions.indexOf((weatherReqInput as HTMLInputElement).value) === -1) {
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=d790a10e5fe74e8db6260020231908&q=${(weatherReqInput as HTMLInputElement).value}&days=2&aqi=no&alerts=no`)
+      .then((response) => response.json())
+      .then((json) => {
 
-      dispatch(addSubmission((weatherReqInput as HTMLInputElement).value));
+        const responseCity = json.location.name;
+        function hasName(element: any): boolean {
+
+          return element.location.name === responseCity;
+
+        }
+
+        if (!apiResponses.some(hasName)) {
+
+          dispatch(addResponse(json));
 
 
-      if (testSubmissions.length !== 0) {
 
-        currSelection = document.getElementsByClassName("selected")[0];
-        newSelection = (document.getElementById("request-history-list") as Element).children[0];
+          if (apiResponses.length !== 0) {
+
+            currSelection = document.getElementsByClassName("selected")[0];
+            newSelection = (document.getElementById("request-history-list") as Element).children[0];
 
 
 
-        newSelection.className = "selected";
-        if (currSelection && currSelection !== newSelection)
-          currSelection.className = "";
+            newSelection.className = "selected";
+            if (currSelection && currSelection !== newSelection)
+              currSelection.className = "";
+
+          }
+
+        }
+
+      });
+
+    /* 1. Add submission to testSubmissions
+
+      if (testSubmissions.indexOf((weatherReqInput as HTMLInputElement).value) === -1) {
+
+        dispatch(addSubmission((weatherReqInput as HTMLInputElement).value));
+
+
+        if (testSubmissions.length !== 0) {
+
+          currSelection = document.getElementsByClassName("selected")[0];
+          newSelection = (document.getElementById("request-history-list") as Element).children[0];
+
+
+
+          newSelection.className = "selected";
+          if (currSelection && currSelection !== newSelection)
+            currSelection.className = "";
+
+        }
 
       }
 
-    }
-
-
-
-
-    /* 1. API Response Fetch
-  
-      fetch(`http://api.weatherapi.com/v1/forecast.json?key=d790a10e5fe74e8db6260020231908&q=${(weatherReqInput as HTMLInputElement).value}&days=2&aqi=no&alerts=no`)
-        .then((response) => response.json())
-        .then((json) => {
-  
-          dispatch(addResponse(json));
-  
-        });
-  
     */
 
   };
