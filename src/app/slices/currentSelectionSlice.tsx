c7779f4dcb2837ed0c209017fffa9019e835a515
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-09-09 09:49:01 -0700

    Update #1: Add a new file that will later be converted into a redux slice file with the name "currentSelectionSlice.tsx".
    Update #2: Add notes to "currentSelectionSlice.tsx"
    that specify what the current selection (which will
    be stored in the redux store) refers to in terms of
    this project.

---
+++ src/app/slices/currentSelectionSlice.tsx
@@ -0,0 +1,12 @@
+/* Notes:
+
+  - The current selection refers to which list item of the 
+    "#weather-request-history" element currently has it's
+    "className" attribute set to "selected".
+  - The current selection should be the index of the selected
+    list item in the "apiResponses" array.
+  - [END]
+
+*/
+
+export { };
