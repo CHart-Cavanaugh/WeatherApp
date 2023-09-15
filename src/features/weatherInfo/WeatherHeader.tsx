9b96d6e8f93b24274df60fbd9ba4dae5dfc7e7ab
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-09-15 07:24:10 -0700

    Update #1: Define functions for each of the values in the "WeatherHeader" component so that when the function is called it returns the appropriate value (or default value), based on what value in the "WeatherHeader" component the returned is associated with and what the current selected city in the "WeatherRequestHistory" component is.
    Update #2: Replace all the placeholder values in the
    "WeatherHeader" component with appropriate function
    calls to the functions defined in "Update #1" (make
    sure that each function used for a value in the
    "WeatherHeader" component matches the label for that
    value).

--- src/features/weatherInfo/WeatherHeader.tsx
+++ src/features/weatherInfo/WeatherHeader.tsx
@@ -1,4 +1,106 @@
+import { useSelector } from "react-redux/es/hooks/useSelector";
+import { AppState } from "../../app/store";
+
+
+
 export function WeatherHeader(): JSX.Element {
+
+  function getCurrentLocation(selectedInfo: any): string {
+
+    const getSelectedLocation: () => string = () => (
+
+      (selectedInfo as { location: { name: string } }).location.name +
+      ", " +
+      (selectedInfo as { location: { country: string } }).location.country
+
+    );
+    const currLocation = selectedInfo ? getSelectedLocation() : "someCity, someCountry";
+
+
+    return currLocation;
+
+  }
+
+  function getCurrentTemp(selectedInfo: any): number {
+
+    const getSelectedTemp: () => number = () => (selectedInfo as { current: { temp_f: number } }).current.temp_f;
+    const currentTemp: number = selectedInfo ? getSelectedTemp() : 0;
+
+
+
+    return currentTemp;
+
+  }
+
+  function getCurrentWindInfo(selectedInfo: any): string {
+
+    const getSelectedWindInfo: () => string = () => (
+
+      `${(selectedInfo as { current: { wind_dir: string } }).current.wind_dir} - ` +
+      `${(selectedInfo as { current: { wind_mph: number } }).current.wind_mph} mph`
+
+    );
+    const currWindInfo: string = selectedInfo ? getSelectedWindInfo() : `"N" - 0 mph`;
+
+
+
+
+
+    return currWindInfo;
+
+  }
+
+  function getCurrentHumidityInfo(selectedInfo: any): string {
+
+    const getSelectedHumidity: () => string = () => `${(selectedInfo as { current: { humidity: number } }).current.humidity} %`;
+    const currHumidity: string = selectedInfo ? getSelectedHumidity() : "0 %";
+
+
+
+    return currHumidity;
+
+  }
+
+  function getCurrentUV(selectedInfo: any): number {
+
+    const getSelectedUV: () => number = () => (selectedInfo as { current: { uv: number } }).current.uv;
+    const currentUV: number = selectedInfo ? getSelectedUV() : 0;
+
+
+
+    return currentUV;
+
+  }
+
+  function getCurrentPressureInfo(selectedInfo: any): string {
+
+    const getSelectedPressure: () => string = () => `${(selectedInfo as { current: { pressure_in: number } }).current.pressure_in} in`;
+    const currPressure: string = selectedInfo ? getSelectedPressure() : "0.00 in";
+
+
+
+    return currPressure;
+
+  }
+
+  function getCurrentVisibilityInfo(selectedInfo: any): string {
+
+    const getSelectedVisibility: () => string = () => `${(selectedInfo as { current: { vis_miles: number } }).current.vis_miles} mi`;
+    const currVisibility: string = selectedInfo ? getSelectedVisibility() : "0.0 mi";
+
+
+
+    return currVisibility;
+
+  }
+
+
+
+  const apiResponses = useSelector((state: AppState) => state.apiResponses);
+  const currentSelection: number = useSelector((state: AppState) => state.currentSelection);
+  const selectedInfo = apiResponses[currentSelection];
+
+
 
   return (
 
@@ -7,7 +109,7 @@ export function WeatherHeader(): JSX.Element {
         <h3 id="location-val"
           className="info-label"
         >
-          someLocation
+          {getCurrentLocation(selectedInfo)}
         </h3>
       </section>
       <section id="main-info">
@@ -35,7 +137,7 @@ export function WeatherHeader(): JSX.Element {
             <p id="current-temp-val"
               className="info-val"
             >
-              0
+              {getCurrentTemp(selectedInfo)}
             </p>
           </div>
         </div>
@@ -51,7 +153,7 @@ export function WeatherHeader(): JSX.Element {
             <p id="wind-val"
               className="info-val"
             >
-              <span>"E"</span> - <span>0</span> <span>mph</span>
+              {getCurrentWindInfo(selectedInfo)}
             </p>
           </div>
           <div id="humidity-info">
@@ -63,7 +165,7 @@ export function WeatherHeader(): JSX.Element {
             <p id="humidity-val"
               className="info-val"
             >
-              <span>0</span> <span>%</span>
+              {getCurrentHumidityInfo(selectedInfo)}
             </p>
           </div>
           <div id="uv-index-info">
@@ -75,7 +177,7 @@ export function WeatherHeader(): JSX.Element {
             <p id="uv-index-val"
               className="info-val"
             >
-              0
+              {getCurrentUV(selectedInfo)}
             </p>
           </div>
           <div id="pressure-info">
@@ -87,7 +189,7 @@ export function WeatherHeader(): JSX.Element {
             <p id="pressure-val"
               className="info-val"
             >
-              <span>0.00</span> <span>in</span>
+              {getCurrentPressureInfo(selectedInfo)}
             </p>
           </div>
           <div id="visibility-info">
@@ -99,7 +201,7 @@ export function WeatherHeader(): JSX.Element {
             <p id="visibility-val"
               className="info-val"
             >
-              <span>0.0</span> <span>mi</span>
+              {getCurrentVisibilityInfo(selectedInfo)}
             </p>
           </div>
         </div>
