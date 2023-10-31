6004a5b53f59521d41a4c37cc6161457c7b42536
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-10-30 17:29:40 -0700

    Move all of the functions, types, and constants that the "HourlyForecast" component depends on to "infoAccessToolkit.tsx" then fix the "HourlyForecast" component so that it uses the "infoAccessToolkit" object from "infoAccessToolkit.tsx".

--- src/features/weatherInfo/infoAccessToolkit.tsx
+++ src/features/weatherInfo/infoAccessToolkit.tsx
@@ -1,9 +1,26 @@
+export type ForecastHour = number;
+export type ForecastHours = [
+
+  hourOne: ForecastHour,
+  hourTwo: ForecastHour,
+  hourThree: ForecastHour,
+  hourFour: ForecastHour,
+  hourFive: ForecastHour
+
+];
+
+
+
 interface Condition {
 
   text: string,
   icon: string,
 
 };
+
+
+
+const DEFAULT_FORECAST_HOURS: ForecastHours = [1, 2, 3, 4, 5];
 
 
 
@@ -170,7 +187,169 @@ function getCurrentVisibilityInfo(selectedInfo: any): string {
 
 
 
+function getTimeZone(selectedInfo: any): string {
+
+  return selectedInfo ? (selectedInfo as { location: { tz_id: string } }).location.tz_id : "Europe/London";
+
+}
+
+function getCurrentHour(selectedInfo: any, currentDate: Date): number {
+
+  return selectedInfo ? currentDate.getHours() : 0;
+
+}
+
+function getCurrentDate(timeZone: string): Date {
+
+  const localDate: Date = new Date();
+  const currentDate = new Date(localDate.toLocaleString("en-US", { timeZone: timeZone }))
+
+
+
+  return currentDate;
+
+}
+
+function getUpdatedHours(selectedInfo: any): ForecastHours {
+
+  const currentHour: number = getCurrentHour(selectedInfo, getCurrentDate(getTimeZone(selectedInfo)));
+  const updatedHours: ForecastHours = DEFAULT_FORECAST_HOURS;
+
+
+
+  for (let i = 0; i < updatedHours.length; i++)
+    updatedHours[i] = (
+
+      currentHour + i < 12 ? currentHour + i
+        : currentHour + i < 24 ? currentHour + i - 12
+          : currentHour + i - 24
+
+    );
+
+
+
+  return updatedHours;
+
+}
+
+function getForecastDays(selectedInfo: any): { date: string }[] {
+
+  return !selectedInfo ? []
+    : (selectedInfo as { forecast: { forecastday: { date: string }[] } }).forecast.forecastday;
+
+}
+
+function isBeforeMidday(selectedInfo: any, hourOffset: number = 0): boolean {
+
+  let currentHour: number = getCurrentHour(selectedInfo, getCurrentDate(getTimeZone(selectedInfo))) + hourOffset;
+
+
+
+  currentHour = currentHour < 24 ? currentHour : currentHour - 24;
+
+
+
+  return currentHour < 12;
+
+}
+
+function getHourlyForecasts(selectedInfo: any, timestamps: ForecastHours): JSX.Element {
+
+  const currentDate = getCurrentDate(getTimeZone(selectedInfo));
+  const currentHour: number = getCurrentHour(selectedInfo, currentDate);
+  const selectedForecastDays: { date: string }[] = getForecastDays(selectedInfo);
+  const forecastDaysObj: { [index: number]: {} } = {};
+
+
+
+  for (let i = 0; i < 2; i++)
+    forecastDaysObj[currentDate.getDate() + i] = selectedForecastDays[i];
+
+
+
+  return (
+
+    <>
+      {timestamps.map((val, index) => {
+
+        const hourlyForecastDay = getCurrentDate(getTimeZone(selectedInfo));
+        const forecastHour = currentHour + index < 24 ? currentHour + index : currentHour + index - 24;
+        let temp: number = 0;
+        let humidity: number = 0;
+        let wind_dir: string = "N";
+        let wind_mph: number = 0;
+
+
+
+        hourlyForecastDay.setHours(forecastHour);
+
+
+
+        temp = !selectedInfo ? 0 : (
+          forecastDaysObj[hourlyForecastDay.getDate()] as { hour: { temp_f: number }[] }
+        ).hour[forecastHour].temp_f;
+        humidity = !selectedInfo ? 0 : (
+          forecastDaysObj[hourlyForecastDay.getDate()] as { hour: { humidity: number }[] }
+        ).hour[forecastHour].humidity;
+        wind_dir = !selectedInfo ? "N" : (
+          forecastDaysObj[hourlyForecastDay.getDate()] as { hour: { wind_dir: string }[] }
+        ).hour[forecastHour].wind_dir;
+        wind_mph = !selectedInfo ? 0 : (
+          forecastDaysObj[hourlyForecastDay.getDate()] as { hour: { wind_mph: number }[] }
+        ).hour[forecastHour].wind_mph;
+
+
+
+
+        return (
+
+          <div key={`hour-forecast-${index}`}>
+            <section key="hourly-condition" className="hourly-info hourly-condition">
+            </section>
+            <section key="hourly-temperature" className="hourly-info hourly-temperature">
+              <h4>Temperature:</h4>
+              <p>{temp} <b>F</b></p>
+            </section>
+            <section key="hourly-humidity" className="hourly-info hourly-humidity">
+              <h4>Humidity:</h4>
+              <p>{humidity} %</p>
+            </section>
+            <section key="hourly-wind" className="hourly-info hourly-wind">
+              <h4>Wind:</h4>
+              <p>
+                <span>{wind_dir}</span> - <span>{wind_mph}</span> mph
+              </p>
+            </section>
+            <footer className="hourly-info hourly-timestamp">
+              <h4>
+                {
+                  val === 0 && isBeforeMidday(selectedInfo, index) ? <span>Midnight</span>
+                    : val === 0 && !isBeforeMidday(selectedInfo, index) ? <span>Noon</span>
+                      : <span><span>{val}</span> <span>{isBeforeMidday(selectedInfo, index) ? "AM" : "PM"}</span></span>
+                }
+              </h4>
+            </footer>
+          </div>
+
+        )
+
+      })}
+    </>
+
+  );
+
+}
+
+
+
 export {
+
+  //CONSTANTS
+  DEFAULT_FORECAST_HOURS,
+
+
+
+  //CURRENT WEATHER INFO FUNCTIONS
   getCurrentLocation,
   getCurrentDay,
   getCurrentTemp,
@@ -179,7 +358,19 @@ export {
   getCurrentHumidityInfo,
   getCurrentUV,
   getCurrentPressureInfo,
-  getCurrentVisibilityInfo
+  getCurrentVisibilityInfo,
+
+
+
+  //HOURLY FORECAST INFO FUNCTIONS
+  getTimeZone,
+  getCurrentHour,
+  getCurrentDate,
+  getUpdatedHours,
+  getForecastDays,
+  isBeforeMidday,
+  getHourlyForecasts,
+
 };
 
 export * as infoAccessToolkit from './infoAccessToolkit';
