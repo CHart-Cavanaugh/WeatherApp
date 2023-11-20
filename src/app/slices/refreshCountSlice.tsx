6e9b3d22c360a8335c047099768443f446268823
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-11-20 04:49:57 -0800

    Define "refreshCountSlice.tsx" to be a new Redux slice with an initial state of "0" and a single action creator called "updateRefreshCount".

---
+++ src/app/slices/refreshCountSlice.tsx
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+const refreshCountSlice = createSlice({
+
+  name: "refreshCount",
+  initialState: 0,
+  reducers: {
+
+    updateRefreshCount: (state) => ++state
+
+  }
+
+});
+
+
+
+export const { updateRefreshCount } = refreshCountSlice.actions;
+
+export default refreshCountSlice.reducer;
