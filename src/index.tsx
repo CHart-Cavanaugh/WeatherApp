c68aa96ee4aae1a9c8e4b1244c7c17acbcabd3ae
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-11-20 09:24:43 -0800

    Deactivate strict mode for app.

--- src/index.tsx
+++ src/index.tsx
@@ -15,11 +15,11 @@ const root: ReactDOM.Root = ReactDOM.createRoot(
 
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
+  // </React.StrictMode>
 );
 
 
